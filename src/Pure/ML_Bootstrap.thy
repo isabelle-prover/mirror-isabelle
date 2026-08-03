f347a683b3b158e12f1adee512a654cfeb7e2091
wenzelm <unknown>
2026-08-03 16:34:46 +0200

    more specific treatment of structure RunCall;

--- src/Pure/ML_Bootstrap.thy
+++ src/Pure/ML_Bootstrap.thy
@@ -9,20 +9,16 @@ imports Pure
 begin
 
 ML \<open>
-  #allStruct ML_Name_Space.global () |> List.app (fn (name, _) =>
-    if member (op =) ML_Name_Space.hidden_structures name then
-      ML (Input.string ("structure " ^ name ^ " = " ^ name))
-    else ());
   structure Output_Primitives = Output_Primitives_Virtual;
   structure Thread_Data = Thread_Data_Virtual;
   structure PolyML = PolyML;
+  structure RunCall = RunCall;
   fun ML_system_pp (_: FixedInt.int -> 'a -> 'b -> PolyML.pretty) = ();
 
   Proofterm.proofs := 0;
 
   Context.setmp_generic_context NONE
     ML \<open>
-      List.app ML_Name_Space.forget_structure ML_Name_Space.hidden_structures;
       structure PolyML =
       struct
         val pointerEq = pointer_eq;
@@ -31,6 +27,7 @@ ML \<open>
         datatype context = datatype PolyML.context;
         datatype pretty = datatype PolyML.pretty;
       end;
+      structure RunCall = struct end;
     \<close>
 \<close>
 
