18968b00e55e325a592d99637d729be67fdcf3f2
Fabian Huch <fabian@huch.de>
2026-09-17 20:58:53 +0200

    more JS operations;

--- src/Pure/General/js.scala
+++ src/Pure/General/js.scala
@@ -26,6 +26,9 @@ object JS {
 
   def value(t: JSON.T): Source = JSON.Format(t)
   def string(s: String): Source = value(s)
+  def boolean(b: Boolean): Source = value(b)
+  def int(i: Int): Source = value(i)
+  def double(d: Double): Source = value(d)
 
   def json_parse(arg: Source): Source = function("JSON.parse", arg)
   def json_print(arg: Source): Source = function("JSON.stringify", arg)
